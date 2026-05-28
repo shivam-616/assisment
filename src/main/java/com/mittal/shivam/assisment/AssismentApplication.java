848777d9419556429652742c59c3135aed951a22
learning_git <shivamittal616@gmail.com>
2026-05-28 19:42:02 +0530

    first commit

---
+++ src/main/java/com/mittal/shivam/assisment/AssismentApplication.java
@@ -0,0 +1,13 @@
+package com.mittal.shivam.assisment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssismentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssismentApplication.class, args);
+    }
+
+}
